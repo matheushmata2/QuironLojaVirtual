d8e8203e58cb500ef72be32b4378287e34e5f29e
matheushmata2 <matheushmata@gmail.com>
2015-08-18 17:58:07 -0300

    Initial 2

---
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    class Produto
+    {
+        public int Produtoid { get; set;}
+
+        public string Descricao { get; set; }
+
+        public string nome { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public string categoria { get; set; }
+    }
+}
